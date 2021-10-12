9fb4b76f34d3574150abf6a84c1741e780e99df6
peroxy <peroxy@users.noreply.github.com>
2021-10-12 17:41:06 +0200

    Add bearer authentication

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import { Strategy } from 'passport-http-bearer';
+import passport from 'passport';
 
 const app = express();
 const port = process.env.SERVER_PORT || 8080;
+//const secretHash = process.env.API_SECRET_HASH || "random";
+
+app.use(passport.initialize());
+
+passport.use(
+  new Strategy(function (token, cb) {
+    return cb(null, 'user');
+  })
+);
 
 // define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello world! hello hehehe wtf');
+app.get('/', passport.authenticate('bearer', { session: false }), (req, res) => {
+  res.send('Hello world!');
 });
 
 // start the Express server
