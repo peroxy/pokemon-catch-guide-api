21743fdb9b55f85e5e5f379e96b4f1a9116acbfe
peroxy <peroxy@users.noreply.github.com>
2021-10-12 17:52:52 +0200

    Add bearer token hash check with bcrypt

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,33 @@
 import express from 'express';
 import { Strategy } from 'passport-http-bearer';
 import passport from 'passport';
+import bcrypt from 'bcryptjs';
 
 const app = express();
+const env = process.env.NODE_ENV || 'development';
 const port = process.env.SERVER_PORT || 8080;
-//const secretHash = process.env.API_SECRET_HASH || "random";
+
+let secretHash: string;
+switch (env) {
+  case 'development':
+    secretHash = bcrypt.hashSync('pokemon');
+    break;
+  case 'production':
+    if (!process.env.API_SECRET_HASH) {
+      throw 'Environment variable API_SECRET_HASH is missing';
+    }
+    secretHash = process.env.API_SECRET_HASH;
+}
 
 app.use(passport.initialize());
 
 passport.use(
   new Strategy(function (token, cb) {
-    return cb(null, 'user');
+    if (bcrypt.compareSync(token, secretHash)) {
+      return cb(null, 'authorized');
+    } else {
+      return cb('could not authorize bearer token', null);
+    }
   })
 );
 
