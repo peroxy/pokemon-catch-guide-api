2974e46a35d431686f75f153d88002deacfcddb6
peroxy <peroxy@users.noreply.github.com>
2021-10-12 17:27:27 +0200

    Add prettier

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = process.env.SERVER_PORT || 8080;
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world! hello hehehe wtf" );
-} );
+app.get('/', (req, res) => {
+  res.send('Hello world! hello hehehe wtf');
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
