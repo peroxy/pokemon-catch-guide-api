583dbae6bc276d817c7c9ad6bfba93b3ff8e7fc6
peroxy <peroxy@users.noreply.github.com>
2021-10-16 13:37:39 +0200

    Add api routes and db methods

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { Strategy } from 'passport-http-bearer';
 import passport from 'passport';
 import bcrypt from 'bcryptjs';
-import { getPokemon } from './db/db';
+import { getEncountersForLocation, getEncountersForPokemon, getPokemonByGeneration, updatePokemonCaught } from './db/db';
 
 const app = express();
 const env = process.env.NODE_ENV || 'development';
@@ -32,13 +32,48 @@ passport.use(
   })
 );
 
-// define a route handler for the default home page
-app.get('/', passport.authenticate('bearer', { session: false }), (req, res) => {
-  res.send('Hello world!');
+app.get('/pokemon/generations/:generation', passport.authenticate('bearer', { session: false }), (req, res) => {
+  const generation = parseInt(req.params.generation);
+  if (generation) {
+    const pokemon = getPokemonByGeneration(generation);
+    res.send(pokemon);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.get('/pokemon/:pokemon_id/encounters', passport.authenticate('bearer', { session: false }), (req, res) => {
+  const pokemonId = parseInt(req.params.pokemon_id);
+  if (pokemonId) {
+    const encounters = getEncountersForPokemon(pokemonId);
+    res.send(encounters);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.get('/locations/:location/generations/:generation/encounters', passport.authenticate('bearer', { session: false }), (req, res) => {
+  const generation = parseInt(req.params.generation);
+  if (generation) {
+    const encounters = getEncountersForLocation(req.params.location, generation);
+    res.send(encounters);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/pokemon/:pokemon_id/caught/:caught', passport.authenticate('bearer', { session: false }), (req, res) => {
+  const pokemonId = parseInt(req.params.pokemon_id);
+  const caught = parseInt(req.params.caught);
+  if (pokemonId && !isNaN(caught)) {
+    const id = updatePokemonCaught(pokemonId, caught > 0);
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // start the Express server
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
-  getPokemon();
 });
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,30 @@
 import db from 'better-sqlite3';
-import { InsertEncounter, InsertPokemon, Pokemon } from './db_models';
+import { Encounter, InsertEncounter, InsertPokemon, Pokemon } from './db_models';
 
 const dbPath = process.env.DATABASE_PATH || './src/db/pokemon.db';
 const database = db(dbPath, { fileMustExist: true });
 
-export const getPokemon = () => {
-  const row = database.prepare('SELECT * FROM pokemon');
-  const neki: Pokemon[] = row.all();
-  return neki;
+export const getPokemonByGeneration = (generation: number): Pokemon[] => {
+  const rows = database.prepare('SELECT * FROM pokemon WHERE generation = ?');
+  return rows.all(generation) as Pokemon[];
+};
+
+export const getEncountersForPokemon = (pokemonId: number): Encounter[] => {
+  const rows = database.prepare('SELECT * FROM encounter WHERE pokemon_id = ?');
+  return rows.all(pokemonId) as Encounter[];
+};
+
+export const getEncountersForLocation = (location: string, generation: number): Encounter[] => {
+  const rows = database.prepare(`
+    SELECT e.* FROM encounter e
+    JOIN pokemon p on e.pokemon_id = p.id
+    WHERE location = ? and generation = ?`);
+  return rows.all(location, generation) as Encounter[];
+};
+
+export const updatePokemonCaught = (pokemonId: number, caught: boolean) => {
+  const insert = database.prepare(`UPDATE pokemon set caught = ? WHERE id = ?`);
+  return insert.run(caught ? 1 : 0, pokemonId).lastInsertRowid;
 };
 
 export const insertPokemon = (pokemon: InsertPokemon) => {
